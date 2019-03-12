fb1b08c9f8123c6fc8293bab6a72dad2d0a97cac
Roberto <roberto@iopatterns.com>
2019-03-12 22:58:06 +0200

    first commit intermusic database

---
+++ lang/en/inter.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_inter
+ * @category    string
+ * @copyright   2019 LMTA <roberto.becerra@lmta.lt>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['choosereadme'] = 'Readme when selecting the theme.';
+$string['inter:edit'] = 'Edit test';
+$string['inter:enrol'] = 'Enrol users';
+$string['inter:manage'] = 'Manage users';
+$string['inter:unenrol'] = 'Unenrol users';
+$string['inter:view'] = 'View test';
+$string['messageprovider:expiry'] = 'Expiry message';
+$string['messageprovider:submission'] = 'Submission message';
+$string['missingidandcmid'] = 'Missing id and cmid';
+$string['modulename'] = 'intermusicdatabase';
+$string['modulename_help'] = 'Use the newmodule module for...';
+$string['modulenameplural'] = 'inter_databases';
+$string['mycustomstring'] = 'You can add \'extra\' strings via the recipe file.';
+$string['mycustomstring2'] = 'Another string with {$a->some} placeholder.';
+$string['newmodulefieldset'] = 'Custom example fieldset.';
+$string['newmodulename'] = 'New Intermusic Database';
+$string['newmodulename_help'] = 'This is the content of the help tooltip associated with the newmodulename field';
+$string['newmodulesettings'] = 'Settings';
+$string['nonewmodules'] = 'No newmodules';
+$string['pluginadministration'] = 'newmodule administration';
+$string['pluginname'] = 'Intermusic Database';
+$string['pluginname_help'] = 'Help text when adding this question type.';
+$string['pluginnameadding'] = 'Rendered when adding this question type.';
+$string['pluginnameediting'] = 'Rendered when editing this question type.';
+$string['pluginnamesummary'] = 'Rendered when choosing the question type to be added.';
+$string['view'] = 'View';
---
+++ version.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin version and other meta-data are defined here.
+ *
+ * @package     mod_inter
+ * @copyright   2019 LMTA <roberto.becerra@lmta.lt>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'mod_inter';
+$plugin->release = '0.0.1';
+$plugin->version = 2019030500;
+$plugin->requires = 2017111300;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->dependencies = [
+    'mod_forum' => ANY_VERSION,
+];
