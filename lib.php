a8b3f2407b2945c00cf0666c7b78c16d0ad00a46
Roberto <roberto@iopatterns.com>
2019-03-14 11:35:33 +0200

    add debug print

--- lib.php
+++ lib.php
@@ -56,8 +56,8 @@ function inter_add_instance($moduleinstance, $mform = null) {
     global $CFG, $DB;
     
     require_once("$CFG->libdir/resourcelib.php");
-    require_once("$CFG->dirroot/mod/inter/locallib.php");
     
+    require_once("$CFG->dirroot/mod/inter/locallib.php");
     $cmid = $moduleinstance->coursemodule;
     $moduleinstance->timecreated = time();
 
@@ -73,7 +73,7 @@ function inter_add_instance($moduleinstance, $mform = null) {
     $completiontimeexpected = !empty($moduleinstance->completionexpected) ? $moduleinstance->completionexpected : null;
     
     \core_completion\api::update_completion_date_event($cmid, 'inter', $id, $completiontimeexpected);
-    
+    echo("<script>console.log('333333333333');</script>");
     //=====================  STORE FILE, TAKEN FROM 'RESOURCE' MODULE =============
 
     return $id;
