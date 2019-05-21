ac949411db9b2632bcc5c7fb4d722892e13740cd
Self <self@Selfs-MacBook-Pro.local>
2019-05-21 13:53:10 +0300

    removed datatables format

--- locallib.php
+++ locallib.php
@@ -397,7 +397,7 @@ function inter_build_html_table($file_url)
                              
                                 if (nxtCol){
                                  nxtCol.style.width = (nxtColWidth - (diffX))+'px';
-                                 console.log('moved'.diffX);
+                                 console.log('moved' + diffX);
                                 }
 
                                 curCol.style.width = (curColWidth + diffX)+'px';
