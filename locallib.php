fc1d861f0472ae66400100947b3ab73db361d047
Self <self@Selfs-MacBook-Pro.local>
2019-09-19 19:29:17 +0300

    add search box

--- locallib.php
+++ locallib.php
@@ -286,6 +286,16 @@ function fill_data_from_csv($file_url, $tablename, $data)
 
 function inter_build_html_table($file_url)
 {
+    //////////////////////////. NEW QUERY //////////////////////
+    $this->config = get_config('resourcespace');
+    $this->resourcespace_api_url = get_config('resourcespace', 'resourcespace_api_url');
+    $this->api_key = get_config('resourcespace', 'api_key');
+    $this->api_user = get_config('resourcespace', 'api_user');
+    $this->enable_help = get_config('resourcespace', 'enable_help');
+    $this->enable_help_url = get_config('resourcespace', 'enable_help_url');
+    //////////////////////////. NEW QUERY //////////////////////    
+
+
     // Detect line breaks, otherwise fgetcsv will return all rows
     ini_set('auto_detect_line_endings', true);
     header('Content-Type: text/html; charset=utf-8');
@@ -550,6 +560,7 @@ function inter_build_html_table($file_url)
     $build .= "<link rel=\"stylesheet\" type=\"text/css\" href=\"".$datatables."\" >";
     $build .= "<script src=\"https://code.jquery.com/jquery-3.3.1.js\"></script>";
     $build .= "<script src=\"js_utilities.js\"></script>";
+    $build .= "<script src=\"sha256.js\"></script>";
 
     $build .= "<br><br><br>";
     $build .= '<div class="topnav">
@@ -562,8 +573,32 @@ function inter_build_html_table($file_url)
     $build .= '<script>
                     function submitMe(id) {
                         var value = document.getElementById(id).value;
-                        alert(value);
+                        
+
+                        var query = "user='.$this->api_user.'&function=do_search";
+
+                        var sha256 = new jsSHA(\'SHA-256\', \'TEXT\');
+                        sha256.update("'.$this->api_key.'"+ query);
+                        var hash = sha256.getHash("HEX");
+
+                        var request_url = "'.$this->resourcespace_api_url.'" + query + "&sign=" + hash;
+
+                        alert(request_url);
                     }
+
+                    String.prototype.hashCode = function() {
+                        var hash = 0;
+                        if (this.length == 0) {
+                            return hash;
+                        }
+                        for (var i = 0; i < this.length; i++) {
+                            var char = this.charCodeAt(i);
+                            hash = ((hash<<5)-hash)+char;
+                            hash = hash & hash; // Convert to 32bit integer
+                        }
+                        return hash;
+                    }
+
                 </script>';
 
     return $build;
