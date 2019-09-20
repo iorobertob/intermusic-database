14d560fb77c3187bb9bcf58ff4097c4b2e2c45a8
Self <self@Selfs-MacBook-Pro.local>
2019-09-21 00:51:57 +0300

    try to access info of course to access instances of poster module

--- locallib.php
+++ locallib.php
@@ -375,7 +375,7 @@ function inter_build_html_table($file_url, $course)
 
     // echo "<script> console.log('DATA ARRAY : ' + '".$data_array."');</script>";
     
-    $length = count($data_array);
+    $length = sizeof($data_array);
     echo "<script> console.log('LENGTH: ' + '".$lenght."');</script>";
     for($i = 0; $i < $length; $i++)
     {
