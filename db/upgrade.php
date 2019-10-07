56df6557196e6d61c80dbd2343fe32b6b5e90ab8
Self <self@Selfs-MacBook-Pro.local>
2019-10-07 12:55:35 +0300

    added premission to access

--- db/upgrade.php
+++ db/upgrade.php
@@ -50,15 +50,14 @@ function xmldb_inter_upgrade($oldversion) {
     	// Define field author to be added to poster.
         $table = new xmldb_table('inter');
         $field = new xmldb_field('platformwide', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null, 'intro');
-    }
 
-    // Conditionally launch add field author.
-    if (!$dbman->field_exists($table, $field)) {
-        $dbman->add_field($table, $field);
-    }
-
-    // Poster savepoint reached.
-    upgrade_mod_savepoint(true, 2019030501, 'inter');
+        // Conditionally launch add field author.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        // Poster savepoint reached.
+        upgrade_mod_savepoint(true, 2019030501, 'inter');
+    } 
 
     return true;
 }
