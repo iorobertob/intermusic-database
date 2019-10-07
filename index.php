77c8659acce22deca986e6f64b7b4e9126dba50d
Self <self@Selfs-MacBook-Pro.local>
2019-10-07 14:42:21 +0300

    added premission to access

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ require_once(__DIR__.'/lib.php');
 $id = required_param('id', PARAM_INT);
 
 $course = $DB->get_record('course', array('id' => $id), '*', MUST_EXIST);
-require_course_login($course);
+require_course_login($course, true);
 
 $coursecontext = context_course::instance($course->id);
 
