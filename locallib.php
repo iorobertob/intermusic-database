e13cd4380e60dcc5146902f2a90e699454bc3e5b
Self <self@Selfs-MacBook-Pro.local>
2019-10-14 12:36:04 +0300

    added tool to print metadata

--- locallib.php
+++ locallib.php
@@ -141,6 +141,8 @@ function get_metadata_from_api($resourcespace_id)
 
     file_print('METADATA:',TRUE);
     file_print(implode(",", $result[1][0]));
+    file_print(implode(",", $result[1][0][0]));
+    file_print(implode(",", $result[1][0]["name"]));
     file_print($result[0]);
     $i = 0;
     foreach($result[1] as $row)
