49e99ebe641729ecff83f016a99f56655d4f4370
Roberto <io_robertob@hotmail.com>
2019-11-11 01:16:14 +0200

    lis debug

--- locallib.php
+++ locallib.php
@@ -147,6 +147,7 @@ function get_metadata_from_api($resourcespace_id, $moduleinstance, $list_metadat
     // file_print("NEW LIST METADATA:", TRUE);
     // file_print("URL:");
     // file_print($result[0]);
+    file_print("tests",true);
     for($i = 0; $i <= sizeof($list_metadata); $i++)
     {
         // $i = 0;
