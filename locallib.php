c36cab7b10c8f997ee9d8b1cce107de650eb7a1c
Roberto <io_robertob@hotmail.com>
2020-01-05 11:16:41 +0200

    modifications to acoomodate for extra column for title in english

--- locallib.php
+++ locallib.php
@@ -48,7 +48,6 @@ function local_test_locallib_function($event)
 /**
  * Get metadata either with an API call or from local moodle modules metadata, serialise it and commit it to DB
  *
- * @param array $data_array is the data array to fill ---- DELETE
  * @param $courseid ID of the course where this list is, used to build mysql queries TODO: change that to native DB calls
  * @param $moduleinstance An instance of the current Inter list that contains information to refer in the API and DB calls
  * @return array $big_array The data coming back from either ResourceSpace or local moodle metadata. 
@@ -135,10 +134,11 @@ function get_poster_list_array($courseid, $moduleinstance)
     // TODO: catch the case when metadatas are incorrect or there is no metadata whatsoever. 
     $list_metadata[0] = ($moduleinstance->meta1 != "" ? $moduleinstance->meta1 : "Composer");
     $list_metadata[1] = ($moduleinstance->meta2 != "" ? $moduleinstance->meta2 : "Title");
-    $list_metadata[2] = ($moduleinstance->meta3 != "" ? $moduleinstance->meta3 : "Surtitle");
-    $list_metadata[3] = ($moduleinstance->meta4 != "" ? $moduleinstance->meta4 : "List");
-    $list_metadata[4] = ($moduleinstance->meta5 != "" ? $moduleinstance->meta5 : "1st Line");
-    $list_metadata[5] = ($moduleinstance->meta6 != "" ? $moduleinstance->meta6 : "Language");
+    $list_metadata[2] = ($moduleinstance->meta3 != "" ? $moduleinstance->meta3 : "Title - English");
+    $list_metadata[3] = ($moduleinstance->meta4 != "" ? $moduleinstance->meta3 : "Surtitle");
+    $list_metadata[4] = ($moduleinstance->meta5 != "" ? $moduleinstance->meta4 : "List");
+    $list_metadata[5] = ($moduleinstance->meta6 != "" ? $moduleinstance->meta5 : "1st Line");
+    $list_metadata[6] = ($moduleinstance->meta7 != "" ? $moduleinstance->meta6 : "Language");
 
     // If flag is on, create a list about all posters in the platform
     // otherwise, only on the posters on the current course. If global, the course number
@@ -150,14 +150,14 @@ function get_poster_list_array($courseid, $moduleinstance)
     $data_array = [];
     if ($moduleinstance->platformwide === "0")
     {
-        $data_array[0] = array ($list_metadata[0], $list_metadata[1], $list_metadata[2], $list_metadata[3], $list_metadata[4], $list_metadata[5], "Content");
-        $query         = "SELECT id, meta_value1, meta_value2, meta_value3, meta_value4, meta_value5, meta_value6, rs_id FROM ".$prefix."poster WHERE course = '".$courseid."'";
+        $data_array[0] = array ($list_metadata[0], $list_metadata[1], $list_metadata[2], $list_metadata[3], $list_metadata[4], $list_metadata[5], $list_metadata[6], "Content");
+        $query         = "SELECT id, meta_value1, meta_value2, meta_value3, meta_value4, meta_value5, meta_value6, meta_value7, rs_id FROM ".$prefix."poster WHERE course = '".$courseid."'";
         $query_modules = "SELECT id, instance FROM ".$prefix."course_modules WHERE (course = '".$courseid."' AND module ='".$poster_id."' AND deletioninprogress ='0' AND visible = '1' )";
     }
     if ($moduleinstance->platformwide === "1")
     {
-        $data_array[0] = array ($list_metadata[0], $list_metadata[1], $list_metadata[2], $list_metadata[3], $list_metadata[4], $list_metadata[5], "Course", "Content");
-        $query         = "SELECT id, meta_value1, meta_value2, meta_value3, meta_value4, meta_value5, meta_value6, rs_id FROM ".$prefix."poster";
+        $data_array[0] = array ($list_metadata[0], $list_metadata[1], $list_metadata[2], $list_metadata[3], $list_metadata[4], $list_metadata[5], $list_metadata[6], "Course", "Content");
+        $query         = "SELECT id, meta_value1, meta_value2, meta_value3, meta_value4, meta_value5, meta_value6, meta_value7, rs_id FROM ".$prefix."poster";
         $query_modules = "SELECT id, instance, course FROM ".$prefix."course_modules WHERE (module ='".$poster_id."' AND deletioninprogress ='0' AND visible = '1' )";
     }
 
@@ -168,17 +168,17 @@ function get_poster_list_array($courseid, $moduleinstance)
     $i = 0;
     while($row = mysqli_fetch_array($result_poster))
     {
-        $posters_array[$i] = array($row[1], $row[2], $row[3], $row[4], $row[5], $row[6]);
+        $posters_array[$i] = array($row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7]);
         $posters_id   [$i] = $row[0];
         $i = $i + 1;
-        $utf = utf8_encode($row[2]);
-        $row[1] = utf8_encode($row[1]);
-        $row[2] = utf8_encode($row[2]);
-        $row[3] = utf8_encode($row[3]);
-        $row[4] = utf8_encode($row[4]);
-        $row[5] = utf8_encode($row[5]);
-        $row[6] = utf8_encode($row[6]);
-
+        // $utf = utf8_encode($row[2]);
+        // $row[1] = utf8_encode($row[1]);
+        // $row[2] = utf8_encode($row[2]);
+        // $row[3] = utf8_encode($row[3]);
+        // $row[4] = utf8_encode($row[4]);
+        // $row[5] = utf8_encode($row[5]);
+        // $row[6] = utf8_encode($row[6]);
+        // $row[7] = utf8_encode($row[7]);
     } 
 
     // Query for the module instances of poster an see which course they are
@@ -201,6 +201,7 @@ function get_poster_list_array($courseid, $moduleinstance)
                                     $posters_array[$key][3] , 
                                     $posters_array[$key][4] ,
                                     $posters_array[$key][5] ,
+                                    $posters_array[$key][6] ,
                                     '<a href=\''.$CFG->wwwroot.'/course/view.php?id=' .$row[2]. '\'>'.$shortname.'</a>',
                                     '<a href=\''.$CFG->wwwroot.'/mod/poster/view.php?id=' .$row[0]. '\'>Poster</a>');
         }
@@ -212,6 +213,7 @@ function get_poster_list_array($courseid, $moduleinstance)
                                     $posters_array[$key][3] , 
                                     $posters_array[$key][4] ,
                                     $posters_array[$key][5] ,
+                                    $posters_array[$key][6] ,
                                     '<a href=\''.$CFG->wwwroot.'/mod/poster/view.php?id=' .$row[0]. '\'>Poster</a>');
         }
         $i = $i + 1;
