1b69c0752d742e0bd747d297ddc0ec4eea8f4d25
Roberto <iorobertob@gmail.com>
2021-01-07 01:48:46 +0200

    backup debug

--- view.php
+++ view.php
@@ -70,8 +70,8 @@ $PAGE->set_heading(format_string($course->fullname));
 $fs = get_file_storage();
 
 $files = $fs->get_area_files($modulecontext->id, 'mod_inter', 'content', 0, 'sortorder DESC, id ASC', false); // TODO: this is not very efficient!!
-var_dump($files);
-die;
+// var_dump($files);
+// die;
 if (count($files) < 1) {
     resource_print_filenotfound($moduleinstance, $cm, $course);
     die;
