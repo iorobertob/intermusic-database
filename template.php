c3a5b88fa1e338de4120843527241a3958ad3e67
Roberto <iorobertob@gmail.com>
2021-01-06 20:52:43 +0200

    added responsive js file

--- template.php
+++ template.php
@@ -5,6 +5,7 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" >
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
 
 <script src="colResizable-1.6.js"></script>
 <script src="js_utilities.js"></script>	
