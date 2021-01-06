f8afedad454b0c70ebb73355095bd9bf17611f1f
Roberto <iorobertob@gmail.com>
2021-01-06 21:09:52 +0200

    cleanup

--- template.php
+++ template.php
@@ -4,8 +4,8 @@
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
 
-<script src="colResizable-1.6.js"></script>
-<script src="js_utilities.js"></script>	
+<!-- <script src="colResizable-1.6.js"></script>
+<script src="js_utilities.js"></script>	 -->
 
 <table class="display  dataTable collapsed dtr-inline" id="the_table">
 	<thead>
