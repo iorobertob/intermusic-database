7137d0282316742eba6483dc00f2870ed8c4d257
Roberto <io_robertob@hotmail.com>
2021-01-08 17:41:26 +0000

    bug fix

--- mod_form.php
+++ mod_form.php
@@ -57,7 +57,7 @@ class mod_inter_mod_form extends moodleform_mod {
 
         $mform->addRule('name', null, 'required', null, 'client');
         $mform->addRule('name', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
-        $mform->addHelpButton('name', 'intername', 'mod_inter');
+        $mform->addHelpButton('name', 'intername', 'inter');
 
         // Adding the standard "intro" and "introformat" fields.
         if ($CFG->branch >= 29) {
